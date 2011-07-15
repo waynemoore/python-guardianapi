4300fc9970929083992ed540194f298cc5addba4
Wayne Moore <wayne@kotega.com>
2011-07-15 11:34:05 +0100

    Extraneous whitepsace cleanup.

--- guardianapi/client.py
+++ guardianapi/client.py
@@ -14,11 +14,11 @@ class Client(object):
         (re.compile('^/content/tags$'), 'tags'),
         (re.compile('^/content/item/(\d+)$'), 'item'),
     )
-    
+
     def __init__(self, api_key, fetcher=None):
         self.api_key = api_key
         self.fetcher = fetcher or fetchers.best_fetcher()
-    
+
     def _do_call(self, endpoint, **kwargs):
         url = '%s?%s' % (
             urlparse.urljoin(self.base_url, endpoint),
@@ -32,7 +32,7 @@ class Client(object):
             else:
                 raise
         return simplejson.loads(response)
-    
+
     def fix_kwargs(self, kwargs):
         kwargs2 = dict([ # underscores become hyphens
             (key.replace('_', '-'), value)
@@ -41,15 +41,15 @@ class Client(object):
         kwargs2['format'] = 'json'
         kwargs2['api_key'] = self.api_key
         return kwargs2
-    
+
     def search(self, **kwargs):
         json = self._do_call('/content/search', **kwargs)
         return SearchResults(self, kwargs, json)
-    
+
     def tags(self, **kwargs):
         json = self._do_call('/content/tags', **kwargs)
         return TagResults(self, kwargs, json)
-    
+
     def item(self, item_id):
         try:
             json = self._do_call('/content/item/%s' % item_id)
@@ -59,7 +59,7 @@ class Client(object):
             else:
                 raise
         return json['content']
-    
+
     def request(self, url):
         "Start with an already constructed URL e.g. apiUrl from a response"
         bits = urlparse.urlparse(url)
@@ -75,44 +75,44 @@ class Client(object):
         if not found_method:
             raise URLNotRecognised(url)
         return getattr(self, found_method)(*args, **kwargs)
-    
+
     def __repr__(self):
         return '<%s: %s>' % (self.__class__.__name__, self.base_url)
 
 class Results(object):
     client_method = None
     default_per_page = 10 # Client library currently needs to know this
-    
+
     def __init__(self, client, kwargs, json):
         self.client = client
         self.kwargs = kwargs
         self.json = json
-    
+
     def __iter__(self):
         return self.results()
-    
+
     def all(self, sleep=1):
         "Iterate over all results, handling pagination transparently"
         return AllResults(self, sleep)
-    
+
     def count(self):
         return 0
-    
+
     def start_index(self):
         return 0
-    
+
     def per_page(self):
         return self.kwargs.get('count', self.default_per_page)
-    
+
     def __getitem__(self, key):
         return self.json[key]
-    
+
     def results(self):
         return []
-    
+
     def has_next(self):
         return self.start_index() + self.per_page() < self.count()
-    
+
     def next(self):
         "Return next Results object in pagination sequence, or None if at end"
         if not self.has_next():
@@ -125,44 +125,44 @@ class Results(object):
         kwargs['count'] = count
         kwargs['start_index'] = start_index + count
         return method(**kwargs)
-    
+
     def __iter__(self):
         for result in self.results():
             yield result
-    
+
     def __repr__(self):
         return '<%s: %d-%d/%d for %r>' % (
             self.__class__.__name__,
-            self.start_index(), self.start_index() + len(self.results()), 
+            self.start_index(), self.start_index() + len(self.results()),
             self.count(), self.kwargs
         )
 
 class SearchResults(Results):
     client_method = 'search'
     default_per_page = 10
-    
+
     def count(self):
         return self.json['search']['count']
-    
+
     def start_index(self):
         return self.json['search']['startIndex']
-    
+
     def results(self):
         return self.json['search']['results']
-    
+
     def filters(self):
         return self.json['search']['filters']
 
 class TagResults(Results):
     client_method = 'tags'
     default_per_page = 10
-    
+
     def count(self):
         return self.json['subjects']['count']
-    
+
     def start_index(self):
         return self.json['subjects']['startIndex']
-    
+
     def results(self):
         return self.json['subjects']['tags']
 
@@ -171,7 +171,7 @@ class AllResults(object):
     def __init__(self, results, sleep=1):
         self.results = results
         self.sleep = sleep
-    
+
     def __iter__(self):
         results = self.results
         while results:
@@ -179,7 +179,7 @@ class AllResults(object):
                 yield result
             time.sleep(self.sleep)
             results = results.next()
-    
+
     def __repr__(self):
         return '<%s: %d for %r>' % (
             self.__class__.__name__, self.results.count(), self.results.kwargs
